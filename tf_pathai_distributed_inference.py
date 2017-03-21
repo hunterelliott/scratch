b82ed3348c3f3d0edee62151d525639e162feb21
Hunter <hunter.elliott@gmail.com>
2017-03-20 20:11:59 -0400

    converted distributed inference script to a sharded style with local batch queues and a shared sample index queue

--- tf_pathai_distributed_inference.py
+++ tf_pathai_distributed_inference.py
@@ -3,6 +3,7 @@ import argparse
 import sys
 import os
 import time
+import numpy as np
 
 # ---- input ---- #
 
@@ -53,6 +54,7 @@ FLAGS, unparsed = parser.parse_known_args()
 
 
 worker_hosts = FLAGS.worker_hosts.split(",")
+n_workers = len(worker_hosts)
 task_ind = FLAGS.task_index
 job_name = FLAGS.job_name
 
@@ -68,20 +70,21 @@ def get_image(file_queue):
   
   reader = tf.WholeFileReader()
   key,image_data = reader.read(file_queue)
-  # image_data = tf.read_file(file_queue)
+  #image_data = tf.read_file(file_name)
   image = tf.image.decode_image(image_data) 
 
 
 
   return image
 
-fileList = os.listdir(FLAGS.data_dir)
-nFiles = len(fileList)
+file_list = os.listdir(FLAGS.data_dir)
+nFiles = len(file_list)
 
 for i in range(nFiles):
-  fileList[i] = os.path.join(FLAGS.data_dir, fileList[i])
+  file_list[i] = os.path.join(FLAGS.data_dir, file_list[i])
 
 batch_size = 32
+shard_size = batch_size*100 #Avoid excessive communications overhead and returns to python by sharding the samples
 
 # ---- create server, populate queue ----- #
 
@@ -92,15 +95,18 @@ batch_size = 32
 cluster = tf.train.ClusterSpec({"ps": [FLAGS.ps_host], "worker":worker_hosts})
 server = tf.train.Server(cluster, job_name=job_name, task_index=task_ind)
 
+#sample_ind = range(nFiles)
+sample_ind = np.arange(0,nFiles)
 
 with tf.device("/job:ps/task:0/cpu:0"):
 
 
   #Initialize the file queues on the parameter server
-  queue = tf.train.string_input_producer(fileList,capacity=batch_size*10,shared_name=
-    'shared_file_queue',num_epochs=1,shuffle=False)
+  queue = tf.train.input_producer(sample_ind,capacity=batch_size*10,shared_name=
+    'shared_sample_queue',num_epochs=1,shuffle=False)
   
   #image = get_image(queue)
+  get_ind = queue.dequeue_many(shard_size)
   #images = tf.train.batch([image],batch_size,num_threads=4,capacity=batch_size*6,shapes=[256,256,3],shared_name='shared_batch_queue')
 
 
@@ -131,9 +137,7 @@ elif job_name == "worker":
   with tf.device("/job:" + job_name + "/task:" + str(task_ind)):
 
 
-    print("Starting worker process " + str(task_ind) + "...")
-
-    image = get_image(queue)
+    print("Starting worker process " + str(task_ind) + "...")    
 
     sess = tf.Session(server.target,config=sess_config)        
     
@@ -142,33 +146,56 @@ elif job_name == "worker":
 
     print("Starting inference...")
     start_time = time.time()
-    try:
+    # try:
     
-      nIm_proc = 0
+    nIm_proc = 0    
+
+    #Get a batch of sample indices from the shared queue for processing
+    inds = sess.run(get_ind)
+
+    print("Processing samples " + str(inds[0]) + " to " + str(inds[-1]))
+
+    #Setup local queue ops for processing
+    shard_list = [file_list[i] for i in inds]      
+    shard_queue = tf.train.string_input_producer(shard_list,capacity=shard_size,num_epochs=1,shuffle=False)
+    image = get_image(shard_queue)
+    images = tf.train.batch([image],batch_size,num_threads=4,capacity=batch_size*3,shapes=[256,256,3])
+
+
+    sess.run(tf.local_variables_initializer()) #Queue runners need local variables initialized
+    threads = tf.train.start_queue_runners(coord=coord,sess=sess)
+
+    print("Shard queue configured")
+
+      #threads = tf.train.start_queue_runners(coord=local_coord,sess=sess)
+      
+
+    try:
+
       while not(coord.should_stop()):
 
-        #Get a batch of images
+        ims = sess.run(images)
 
-        #ims = sess.run(tf.image.resize_images(images,[128, 128]))
-
-
-        im = sess.run(image)
-
+        #im = sess.run(get_image(file_list[ind]))
+        #qs = sess.run(shard_queue.size())
+        #print(qs)
         #Run inference on them
         #pred = sess.run(predict)
 
 
-        #nIm_proc += ims.shape[0]
-        nIm_proc += 1
+        nIm_proc += ims.shape[0]
+      #nIm_proc += 1
 
-        if nIm_proc%(10*batch_size) == 0:
+        if nIm_proc%(2*batch_size) == 0:
           print("Finished " + str(nIm_proc) + " images")
 
     except tf.errors.OutOfRangeError:
-      print("Done! Completed " + str(nIm_proc) + " images total")
+      print("Shard queue exhausted, processed " + str(nIm_proc) + " samples total")
 
-    finally:
-      coord.request_stop()
+    #   print("Queue exhausted. Completed " + str(nIm_proc) + " images total")
+
+    #finally:
+      #coord.request_stop()
 
     end_time = time.time()
     print("Done, took " + str(end_time-start_time) + " seconds")
