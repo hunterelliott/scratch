b891911b3c70790eed97f1dfe76bebe6150d7043
Hunter Elliott <none>
2017-07-19 19:14:21 -0400

    refactored iteration methods into module

--- iteration_tests.py
+++ iteration_tests.py
@@ -6,63 +6,27 @@ import numpy as np
 from keras import backend as K
 import matplotlib.animation as animation
 
-
-def transition_model(n_t,input_shape):
-
-    x = Input(shape=input_shape)
-
-    transition_dim = 16
-    encode = Conv2D(transition_dim,5,padding='same',activation='tanh',name='encode_conv')
-    decode = Conv2D(input_shape[2], 5, padding='same', activation='tanh',name='decode_conv')
-
-    x_prime = x
-    for i in range(n_t):
-        h = encode(x_prime)
-        x_prime = decode(h)
-
-    model = Model(inputs=x, outputs=x_prime)
-
-    return model
+import iterations as I
 
 n_t = 32
 
 im_shape = (256,256,3)
 
-C = transition_model(n_t,im_shape)
+C = I.transition_model(n_t,im_shape)
 
-#im = np.random.normal(0,1,(1,)+im_shape)
-im = np.zeros((1,) + im_shape)
-im[0,128,128,:] = 1
-
-
-im2 = C.predict(im)
-
-
-plt.imshow(np.squeeze(im2))
-
-fig = plt.figure(2)
+#X_t0 = np.random.normal(0,1,(1,)+im_shape)
+X_t0 = np.zeros((1,) + im_shape)
+X_t0[0,128,128,:] = 1
 
 
 
-def get_t(t):
-    global C,im,n_t
-    ts = [C.nodes_by_depth[depth][0].output_tensors[0] for depth in C.nodes_by_depth if
-          C.nodes_by_depth[depth][0].output_tensors[0].name.find('decode') >= 0]
-    C_intermediate = K.function([C.layers[0].input], [ts[n_t-t-1]])
-    im_int = np.squeeze(C_intermediate([im])[0])
-    return im_int
 
-anim_i = 0
-im_han = plt.imshow(get_t(anim_i))
+fig = plt.figure()
+t_0 = 0
+im_han = plt.imshow(I.get_X_t(t_0,C,X_t0))
 
-def update_anim(*args):
+ani = animation.FuncAnimation(fig,I.update_anim,fargs=(im_han,C,X_t0),frames=31,interval = 250)
 
-    global anim_i
-    anim_i += 1
-    im_han.set_array(get_t(anim_i))
-
-
-ani = animation.FuncAnimation(fig,update_anim,frames=31,interval = 250)
 
 plt.show()
 jk=1
