1dcb9ded5aad7b8d261f8fb5d0ac458f0e8d6d6c
Hunter Elliott <none>
2017-08-02 22:18:10 -0400

    optimization improvements - now using generators

--- iteration_tests.py
+++ iteration_tests.py
@@ -22,37 +22,37 @@ im_shape = (256,256,3)
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 #Optimizer params
-lr = 5e-7 #Works OK for unsigned conservation
+lr = 1e-6 #Works OK for unsigned conservation
 #lr = 1e-3
+lr_decay = 1e-10
 lr_decay = 0
-momentum = 0.5
-n_epochs = int(3)
+momentum = .5
+batch_size = 256
+n_epochs = 1
+n_batch_per_epoch = 4096
+
 
 # ---- Initialization --- #
 
 C = I.transition_model(im_shape)
 
-
+#Create X_0 for visualization
 #X_t0 = np.random.normal(0,1,(1,)+im_shape)
 X_t0 = np.zeros((1,) + im_shape)
 X_t0[0,128,128,:] = 1
 
-n_X_train = 4096
-n_particle_per_t = 3
-thresh = 1-n_particle_per_t/np.prod(im_shape)
-X_t_0_train = (np.random.uniform(0,1,(n_X_train,) + im_shape) > thresh).astype(np.float32)
+#optimizer = optimizers.SGD(lr=lr,decay=lr_decay,momentum=momentum)
+optimizer = optimizers.Adam(lr=lr,decay=lr_decay)
+#optimizer = optimizers.rmsprop(lr=lr,decay=lr_decay)
+C.compile(loss=I.conservation_term,optimizer=optimizer)
+cb = [callbacks.TensorBoard(log_dir='/home/hunter/Desktop/TEMP/tmp_tensorboard1',write_graph=False)]
+#cb = cb + [callbacks.ReduceLROnPlateau(monitor='loss',factor=.75)]
 
 
 # ---- Optimization ---- #
 
 # Optimize the transition model
-#optimizer = optimizers.SGD(lr=lr,decay=lr_decay,momentum=momentum)
-optimizer = optimizers.rmsprop(lr=lr,decay=lr_decay)
-C.compile(loss=I.conservation_term,optimizer=optimizer)
-
-cb = [callbacks.TensorBoard(log_dir='/home/hunter/Desktop/TEMP/tmp_tensorboard1',write_graph=False)]
-
-#C.fit(x=X_t_0_train,y=X_t_0_train,epochs=n_epochs,batch_size=384,callbacks=cb)
+C.fit_generator(I.generate_train_X(im_shape,batch_size),n_batch_per_epoch,epochs=n_epochs,callbacks=cb,max_q_size=128)
 
 # ---- Iteration ----- #
 
@@ -67,9 +67,21 @@ im_han = plt.imshow(I.scale_im(I.get_X_t(t_0,R,X_t0)) ,clim=(-1, 1))
 plt.colorbar()
 ani = animation.FuncAnimation(fig,I.update_anim,fargs=(im_han,R,X_t0),frames=n_t,interval = 250)
 
+# ---- Trend visualization --- #
+
+tot_mass = np.zeros(n_t)
+
+for t in range(n_t):
+
+    X_t = I.get_X_t(t,R,X_t0)
+    tot_mass[t] = np.sum(X_t)
+
+fig = plt.figure(2)
+plt.plot(tot_mass)
+
+
 
 plt.show()
-
 jkl=1
 #TODO - define objective function for ||dXdt|| as tf tensor, use as custom loss
 
--- iterations.py
+++ iterations.py
@@ -30,26 +30,42 @@ def iteration_model(C,n_t,input_shape):
 
     return model
 
-def get_X_t(t,C,X_t_0):
+def get_X_t(t, R, X_t_0):
 
 
-    ts = [C.nodes_by_depth[depth][0].output_tensors[0] for depth in C.nodes_by_depth if
-          C.nodes_by_depth[depth][0].output_tensors[0].name.find('decode') >= 0]
+    ts = [R.nodes_by_depth[depth][0].output_tensors[0] for depth in R.nodes_by_depth if
+          R.nodes_by_depth[depth][0].output_tensors[0].name.find('decode') >= 0]
     n_t = len(ts)
-    C_intermediate = K.function([C.layers[0].input], [ts[n_t-t-1]])
+    C_intermediate = K.function([R.layers[0].input], [ts[n_t - t - 1]])
     X_t = np.squeeze(C_intermediate([X_t_0])[0])
     return X_t
 
 def conservation_term(X_t_0,X_t_1):
 
     #Absolute rate of change of mass
-    dMdt = tf.abs(tf.reduce_sum(X_t_0) -  tf.reduce_sum(X_t_1))
+    dMdt = tf.abs(tf.reduce_sum(tf.reduce_sum(tf.reduce_sum(X_t_0, 3), 2), 1) -
+                  tf.reduce_sum(tf.reduce_sum(tf.reduce_sum(X_t_1, 3), 2), 1))
 
     return dMdt
 
+def get_train_X(X_shape,n_X_train):
+
+    n_particle_per_t = 3
+    thresh = 1-n_particle_per_t/np.prod(X_shape)
+    X_t_0_train = (np.random.uniform(0,1,(n_X_train,) + X_shape) > thresh).astype(np.float32)
+
+    return X_t_0_train
+
+def generate_train_X(X_shape,batch_size):
+
+    while True:
+        X = get_train_X(X_shape,batch_size)
+        yield (X,X)
+
+
 def scale_im(X):
 
-    contrast = 1.5
+    contrast = 1.0
     X = ((X * contrast) + 1) / 2
 
     return X
