c8f6fb18479d4c983df701aef14d72e0a11d9fba
Hunter <elliott.hunter@gmail.com>
2017-10-01 12:54:07 -0400

    Added MNIST tests with MLP

--- cian_cnn/cian_cnn_testing.m
+++ cian_cnn/cian_cnn_testing.m
@@ -1,32 +1,75 @@
-%% --- Define layer array --- %%
+%% --- Define dataset --- %%
 
-nClasses = 3;
-nSamples = 6;
-inputDims = 10;
+datasetName = 'MNIST'
 
-%Setup an easy classification problem
+switch datasetName
 
-labels = false(nClasses,nSamples);
-labels(1,1:2) = true;
-labels(2,3:4) = true;
-labels(3,5:6) = true;
-[~,labelInd] = max(labels,[],1);
+    case 'Trivial'
+        
+        nClasses = 3;
+        nSamples = 6;
+        inputDims = 10;
 
-classMeans = linspace(0,nClasses*2,nClasses);
-input = randn(inputDims,nSamples);
-for j = 1:nSamples
-    input(:,j) = input(:,j) + classMeans(labels(:,j));    
+        %Setup an easy classification problem
+
+        labels = false(nClasses,nSamples);
+        labels(1,1:2) = true;
+        labels(2,3:4) = true;
+        labels(3,5:6) = true;
+        [~,labelInd] = max(labels,[],1);
+
+        classMeans = linspace(0,nClasses*2,nClasses);
+        input = randn(inputDims,nSamples);
+        for j = 1:nSamples
+            input(:,j) = input(:,j) + classMeans(labels(:,j));    
+        end
+
+        layerDims = [inputDims ./ 2 .^ (0:1), nClasses];
+        
+    case 'MNIST'
+        
+        mnistPath = fullfile(matlabroot,'toolbox','nnet','nndemos',...
+                            'nndatasets','DigitDataset');
+        mnistData = imageDatastore(mnistPath,...
+                'IncludeSubfolders',true,'LabelSource','foldernames');
+
+        labelInd = double(mnistData.Labels);
+        nClasses = numel(unique(labelInd));
+        nSamples = numel(labelInd);
+        labels = false(nClasses,nSamples);
+        labels(sub2ind(size(labels),labelInd',1:nSamples)) = true;
+        input = mnistData.readall;
+        input = cellfun(@(x)(x(:)),input,'Unif',0);
+        
+        input = double([input{:}]);
+        input = input - 128;
+        input = input / 128;
+        
+        inputDims = size(input,1);
+        
+        layerDims = round([inputDims ./ 2 .^ (0:3), nClasses]);
+        
+        
 end
-
+    
+%% --- Define network --- %%
 
 %Create simple MLP
 
-layerDim = 6;
-layers = {InnerProductLayer(randn(layerDim,inputDims),ones(layerDim,1))};
+biasInit = 0.1;
+wVarInit = 0.01;
 
-layers = [layers {ReLULayer()}];
+nLayers = numel(layerDims);
+layers = {};
 
-layers = [layers {InnerProductLayer(randn(nClasses,layerDim),ones(nClasses,1))}];
+for iLayer = 1:nLayers-1
+
+    layers = [layers {InnerProductLayer(randn(layerDims(iLayer+1),layerDims(iLayer))*wVarInit,biasInit*ones(layerDims(iLayer+1),1))}];
+    
+    if iLayer < nLayers-1
+        layers = [layers {ReLULayer()}];
+    end
+end
 
 layers = [layers {SoftmaxLayer()}];
 
@@ -37,28 +80,41 @@ layers = [layers {CrossEntropyLayer()}];
 
 %% --- Train it ---- %%0
 
-nIters = 1e4;
-learningRate = 1e-4;
+nIters = 1e3;
+learningRate = 1e-2;
+batchSize = 64;
 
 lossPerIter = nan(nIters,1);
 accPerIter = nan(nIters,1);
 
+%Randomize sample order
+sampleInds = randperm(nSamples);
+
 for i = 1:nIters
     
-    [preds,losses] = cianForward(layers,input,labels);
+    %Get the batch
+    currIndInd = mod((i-1)*batchSize+1:i*batchSize,nSamples)+1;
+    currInd = sampleInds(currIndInd);
+    currInput = input(:,currInd);
+    currLabels = labels(:,currInd);
+    currLabelInd = labelInd(currInd);   
+    
+    %Make the forward and backward passes and the update
+    [preds,losses] = cianForward(layers,currInput,currLabels);
     gradInput = cianBackward(layers);
     cianUpdate(layers,learningRate)
     
-    lossPerIter(i) = mean(losses);
-    
+    %Log performance
+    lossPerIter(i) = mean(losses);    
     [~,predLabelInd] = max(preds,[],1);
     [~,predLabel] = max(preds,[],1);
-    accPerIter(i) = nnz(predLabelInd == labelInd) / nSamples;
+    accPerIter(i) = double(nnz(predLabelInd(:) == currLabelInd(:))) / batchSize;
     
-    if mod(i,100) == 0
+    if i == 1 || mod(i,10) == 0
         disp(['Iteration ' num2str(i) ', loss = ' num2str(lossPerIter(i)) ', accuracy = ' num2str(accPerIter(i))])
     end
 end
+
 cf = figure;
 subplot(2,1,1)
 semilogy(lossPerIter)
@@ -67,4 +123,4 @@ subplot(2,1,2)
 plot(accPerIter)
 xlabel('Iteration')
 ylabel('Accuracy')
-[preds,losses] = cianForward(layers,input,labels)
+%[preds,losses] = cianForward(layers,input,labels)
