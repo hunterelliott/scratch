fbb30434eed36ef2e48b1f8d41baa1e61c5c0881
Hunter Elliott <none>
2017-11-18 12:22:47 -0500

    playing with AE webcam prediction into future

--- webcam_train_ae.py
+++ webcam_train_ae.py
@@ -9,7 +9,7 @@ from keras import backend as K
 
 
 
-cv2.startWindowThread()
+#cv2.startWindowThread()
 in_window = "input"
 cv2.namedWindow(in_window,cv2.WINDOW_KEEPRATIO)
 win_size = (600,600,3)
@@ -37,8 +37,11 @@ cv2.resizeWindow(pred_window,600,600)
 
 AE = Sequential()
 
-n_coder_layers = 8
-layer_base_dim = 4
+# n_coder_layers = 8
+# layer_base_dim = 4
+n_coder_layers = 2
+layer_base_dim = 8
+
 
 AE.add(Conv2D(im_shape[-1],3,strides=(2,2),input_shape=im_shape))
 
@@ -87,18 +90,25 @@ def webcam_im_generator(model):
     while keep_fuckin_goin:
         keep_fuckin_goin,im = vc.read()
         cv2.imshow(in_window, im)
-        #im = im[:,:,::-1]
-        #key = cv2.waitKey(50)
-        im_out = pre_process_im(im)
 
+        key = cv2.waitKey(20)
+        im1 = pre_process_im(im)
+
+        for t in range(10):
+            keep_fuckin_goin, im = vc.read()
+        im2 = pre_process_im(im)
 
         with graph.as_default():
 
-            im_pred = model.predict([im_out])
+            im_pred = model.predict([im2])
             im_pred = de_process_im(im_pred)
             cv2.imshow(pred_window,im_pred)
         i_iter = i_iter + 1
-        yield (im_out,im_out)
+
+        if key == 27:
+            break
+
+        yield (im1,im2)
 
 
 AE.fit_generator(webcam_im_generator(AE),steps_per_epoch=10,epochs=1e3)
