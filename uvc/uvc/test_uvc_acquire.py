e026f66fb97b6954fcd4aa037a29ef30f0fc6641
Hunter Elliott <none>
2024-12-28 18:18:00 -0700

    now using gpu and handling normalization

--- uvc/uvc/test_uvc_acquire.py
+++ uvc/uvc/test_uvc_acquire.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from screeninfo import get_monitors
 import torch
+from torchvision.transforms import Normalize
 
 
 CAM_DEVICE = 0
@@ -21,11 +22,11 @@ cv.setWindowProperty(WIN_NAME, cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
 cv.moveWindow(WIN_NAME, get_monitors()[MON_NUM].x, get_monitors()[MON_NUM].y)
 
 assert cap.isOpened()
+assert torch.cuda.is_available()
 
-
-
+device = torch.device("cuda")
 model =  torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet',
-    in_channels=3, out_channels=3, init_features=32, pretrained=False)
+    in_channels=3, out_channels=3, init_features=32, pretrained=False).to(device)
 
 
 
@@ -41,18 +42,23 @@ while True:
 
     frame_rs = cv.resize(frame,(256,256))
 
-    model_in = torch.tensor(frame_rs).float().movedim(2, 0).unsqueeze(0)
+    model_in = torch.tensor(frame_rs).float().movedim(2, 0).unsqueeze(0).to(device)
+    in_mean = torch.mean(model_in,(2,3), keepdim=True)
+    in_std = torch.std(model_in,(2,3), keepdim=True)
+    model_in = model_in - in_mean
+    model_in = model_in / (in_std + 1e-5)
 
-    model_out = model(model_in).squeeze().movedim(0,2).detach().numpy().astype(np.uint8)
+    model_out = model(model_in).detach()
+
+    model_out = model_out * in_std
+    model_out = model_out + in_mean
+    model_out = model_out.cpu().squeeze().movedim(0,2).numpy().astype(np.uint8)
 
     out_rs = cv.resize(model_out, RESOLUTION)
 
     # Display the resulting frame
     # cv.imshow(WIN_NAME, frame)
     cv.imshow(WIN_NAME, out_rs)
-
-    # TODO - handle normalziation
-    # TODO - move to GPU so we're getting acceleration
 
 
     if cv.waitKey(1) == ord('q'):
